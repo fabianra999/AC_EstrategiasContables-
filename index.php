b6828cd9f535930dbfceb024c2fd4953cb74342b
desbog7 <desbog7@DESBOG7>
2017-09-06 14:28:26 -0500

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<!--head-->
+	<?php include 'view/modules/head.php';?>
+	<body>
+      <input type="text" id="add-task" placeholder="tarea">
+      <ul id="tasks-container">
+      </ul>
+      
+      
+      <ul id="menu">
+        <li><a href="https://codepen.io/fabianra999/pen/ZJmLEm?editors=1111">item_1</a></li>
+        <li><a href="https://codepen.io/fabianra999/pen/ZJmLEm?editors=1111">item_2</a></li>
+        <li><a href="https://codepen.io/fabianra999/pen/ZJmLEm?editors=1111">item_3</a></li>
+        <li><a href="https://codepen.io/fabianra999/pen/ZJmLEm?editors=1111">item_4</a></li>
+        <li><span>1</span><b>2</b><a href="https://codepen.io/fabianra999/pen/ZJmLEm?editors=1111">item_5</a></li>
+      </ul>
+
+		<!--header-->
+		<?php include 'view/modules/header.php';?>
+		<!--footer-->
+		<?php include 'view/modules/footer.php';?>
+		<!--scripts-->
+		<?php include 'view/modules/scripts.php';?>
+	</body>
+</html>
+
+
+
+
---
+++ view/modules/scripts.php
@@ -0,0 +1,13 @@
+<!-- Script -->
+
+<!--Components-->
+<script src="dist/components/js/components.js"></script>
+<!--javaScript-->
+<script src="dist/scripts/javaScript/main.js"></script>
+<!--typeScript-->
+<script src="dist/scripts/tipeScript/scriptTs.js"></script>
+
+
+<!-- bower:js -->
+
+<!-- endbower -->
